79ece76f58ed70436071ef5b21d0b241063720ec
jjtey <junjietey@gmail.com>
2022-06-05 23:14:58 +0800

    Added 1 instance of refactoring for input validation

--- scriptReversed.js
+++ scriptReversed.js
@@ -1,10 +1,9 @@
 let choices = ["scissors", "paper", "stone"]; //global array of choices, sorted such that choices[i] wins choices[i+1] and choices[n] wins choices[0] (wrap around)
 const reversedChoices = [
-  //array is reversed but sorted where choices[i] wins choices[i+1] and choices[n] wraps around to win choices[0], no need to change logic from normal scissors, paper, stone.
   "reversed stone",
   "reversed paper",
   "reversed scissors",
-];
+]; //array is reversed but sorted where choices[i] wins choices[i+1] and choices[n] wraps around to win choices[0]
 
 var gamesWon = 0;
 var gamesPlayed = 0;
@@ -13,23 +12,15 @@ var userName = "";
 var main = function (input) {
   if (!userName) {
     if (!input) {
-      return `Hi, please input your user name!`;
+      var gameInstructions = `Hi, please input your user name!`;
+      return gameInstructions;
     }
     userName = input;
-    return `Hi ${userName}, welcome to Scissors Paper Stone! ✌️✋👊<br><br> To play, input "Scissors", "Paper" or "Stone" to test your luck!<br><br>Alternatively, input "Reversed Scissors", "Reversed Paper" or "Reverse Stone" to try your hand at Reversed Scissors Paper Stone!<br><br>Good luck!`;
+    var gameInstructions = `Hi ${userName}, welcome to Scissors Paper Stone! ✌️✋👊<br><br> To play, input "Scissors", "Paper" or "Stone" to test your luck!<br><br>Alternatively, input "Reversed Scissors", "Reversed Paper" or "Reverse Stone" to try your hand at Reversed Scissors Paper Stone!<br><br>Good luck!`;
+    return gameInstructions;
   }
   var inputLower = input.toLowerCase();
-  if (choices.includes(inputLower)) {
-    //compares input with array elements
-    var inputValidity = `${userName}, your input is valid`;
-  } else if (reversedChoices.includes(inputLower)) {
-    //if input matches that of 'reversed' array, we use the sorted 'reversed' array.
-    var inputValidity = `${userName}, your input is valid, reversed rules apply!`;
-    choices = reversedChoices;
-  } else {
-    var inputValidity = `${userName}, your input is invalid. Please input "scissors", "paper" or "stone"!`;
-    return inputValidity;
-  }
+  var inputValidity = checkInputValidity(inputLower, userName);
   var randomChoice = chooseRandom(); //program's random choice
   var choicesOutcome = compareChoices(inputLower, randomChoice); //generate win/loss/draw results
   var myOutputValue = `${inputValidity}.<br><br>You picked ${inputLower} ${addEmoji(
@@ -39,6 +30,22 @@ var main = function (input) {
   )}.<br><br>${choicesOutcome}<br><br> You've won ${gamesWon} out of ${gamesPlayed} turn(s) so far!`;
   return myOutputValue;
 };
+
+function checkInputValidity(inputLower, userName) {
+  if (choices.includes(inputLower)) {
+    //compares input with array elements
+    var inputValidity = `${userName}, your input is valid`;
+    return inputValidity;
+  } else if (reversedChoices.includes(inputLower)) {
+    //if input matches that of 'reversed' array, we use the sorted 'reversed' array.
+    var inputValidity = `${userName}, your input is valid, reversed rules apply!`;
+    choices = reversedChoices;
+    return inputValidity;
+  } else {
+    var inputValidity = `${userName}, your input is invalid. Please input "scissors", "paper" or "stone"!`;
+    return inputValidity;
+  }
+}
 
 function chooseRandom() {
   var randomDecimal = Math.random() * 2; //returns 0 to 2 (exclusive)
