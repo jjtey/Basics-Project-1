48cf7e5026ea3153fbb90bcf3efd84e953459b37
jjtey <junjietey@gmail.com>
2022-06-06 21:19:03 +0800

    Corrected implementation to exclude invalid inputs from win/loss calculations

--- scriptReversed.js
+++ scriptReversed.js
@@ -1,10 +1,7 @@
-let choices = ["scissors", "paper", "stone"]; //global array of choices, sorted such that choices[i] wins choices[i+1] and choices[n] wins choices[0] (wrap around)
-const reversedChoices = [
-  "reversed stone",
-  "reversed paper",
-  "reversed scissors",
-]; //array is reversed but sorted where choices[i] wins choices[i+1] and choices[n] wraps around to win choices[0]
+let normal = ["scissors", "paper", "stone"]; //global array of choices, sorted such that choices[i] wins choices[i+1] and choices[n] wins choices[0] (wrap around)
+const reversed = ["reversed stone", "reversed paper", "reversed scissors"]; //array is reversed but sorted where choices[i] wins choices[i+1] and choices[n] wraps around to win choices[0]
 
+var choices = "";
 var gamesWon = 0;
 var gamesPlayed = 0;
 var userName = "";
@@ -21,25 +18,33 @@ var main = function (input) {
   }
   var inputLower = input.toLowerCase();
   var inputValidity = checkInputValidity(inputLower, userName);
-  var randomChoice = chooseRandom(); //program's random choice
-  var choicesOutcome = compareChoices(inputLower, randomChoice); //generate win/loss/draw results
-  var myOutputValue = `${inputValidity}.<br><br>You picked ${inputLower} ${addEmoji(
-    inputLower
-  )}, the program picked ${randomChoice} ${addEmoji(
-    randomChoice
-  )}.<br><br>${choicesOutcome}<br><br> You've won ${gamesWon} out of ${gamesPlayed} turn(s) so far!`;
-  return myOutputValue;
+  if (
+    inputValidity ==
+    `${userName}, your input is invalid. Please input "scissors", "paper" or "stone"!`
+  ) {
+    return inputValidity;
+  } else {
+    var randomChoice = chooseRandom(); //program's random choice
+    var choicesOutcome = compareChoices(inputLower, randomChoice); //generate win/loss/draw results
+    var myOutputValue = `${inputValidity}.<br><br>You picked ${inputLower} ${addEmoji(
+      inputLower
+    )}, the program picked ${randomChoice} ${addEmoji(
+      randomChoice
+    )}.<br><br>${choicesOutcome}<br><br> You've won ${gamesWon} out of ${gamesPlayed} turn(s) so far!`;
+    return myOutputValue;
+  }
 };
 
 function checkInputValidity(inputLower, userName) {
-  if (choices.includes(inputLower)) {
+  if (normal.includes(inputLower)) {
     //compares input with array elements
     var inputValidity = `${userName}, your input is valid`;
+    choices = normal;
     return inputValidity;
-  } else if (reversedChoices.includes(inputLower)) {
+  } else if (reversed.includes(inputLower)) {
     //if input matches that of 'reversed' array, we use the sorted 'reversed' array.
     var inputValidity = `${userName}, your input is valid, reversed rules apply!`;
-    choices = reversedChoices;
+    choices = reversed;
     return inputValidity;
   } else {
     var inputValidity = `${userName}, your input is invalid. Please input "scissors", "paper" or "stone"!`;
@@ -48,8 +53,8 @@ function checkInputValidity(inputLower, userName) {
 }
 
 function chooseRandom() {
-  var randomDecimal = Math.random() * 2; //returns 0 to 2 (exclusive)
-  var randomInteger = Math.round(randomDecimal); //returns 0 to 2 (inclusive)
+  var randomDecimal = Math.random() * 3; //returns 0 to 2 (exclusive)
+  var randomInteger = Math.floor(randomDecimal); //returns 0 to 2 (inclusive)
   var randomChoice = choices[randomInteger];
   return randomChoice;
 }
@@ -89,7 +94,7 @@ function addEmoji(inputLower) {
     var inputLowerEmoji = String.fromCodePoint(0x1f9fb);
     return inputLowerEmoji;
   } else if (inputLower == "stone" || inputLower == "reversed stone") {
-    var inputLowerEmoji = String.fromCodePoint(0x1faa8);
+    var inputLowerEmoji = String.fromCodePoint(0x1f5ff);
     return inputLowerEmoji;
   }
 }
